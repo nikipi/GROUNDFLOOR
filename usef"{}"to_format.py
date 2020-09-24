fd903ae1e1aa18608e0c555f2e0bcfbc4344eca1
Yulu (Niki) Pi <61734290+nikipi@users.noreply.github.com>
2020-09-24 11:12:22 +0200

    Update usef"{}"to_format.py

--- usef"{}"to_format.py
+++ usef"{}"to_format.py
@@ -4,3 +4,9 @@ weight=input('How much do you weight:')
 height=input('How tall are you:')
 
 print(f"So you are {age} old,{height} cm tall and {weight}kg heavy")
+
+age=['11','34']
+weight=['12','12']
+
+for i,j in age,weight:
+    print('So you are ' + i + ' old,'+ j + 'cm heavy')
