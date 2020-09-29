50f4b9a24a66abc1084040e9f1107c3cd5913f30
Yulu (Niki) Pi <61734290+nikipi@users.noreply.github.com>
2020-09-29 15:54:50 +0200

    Update upper_first_letter.py

--- upper_first_letter.py
+++ upper_first_letter.py
@@ -7,6 +7,15 @@ S=list (map(format_name, ['adam', 'LISA', 'barT']))
 print(S)
 
 
+my_name = 'Yulu Pi'
+
+def upper_first(name):
+    first_space= name.find(' ')
+    return name[0:first_space].upper()+name[first_space:]
+
+print(upper_first(my_name))
+
+
 
 
 
