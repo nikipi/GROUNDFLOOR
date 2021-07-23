7fb6d9d2cba7c9fda5a615359704bd94af718338
Yulu (Niki) Pi <61734290+nikipi@users.noreply.github.com>
2021-07-23 17:43:06 +0800

    Update usef"{}"to_format.py

--- usef"{}"to_format.py
+++ usef"{}"to_format.py
@@ -10,3 +10,29 @@ weight=['12','12']
 
 for i,j in age,weight:
     print('So you are ' + i + ' old,'+ j + 'cm heavy')
+    
+    
+    
+  def tag(name,*content,cls=None,**attrs):
+    if cls is not None:
+        attrs['class']=cls
+    if attrs:
+        attr_str=''.join(' %s="%s"'%(attr,value) for attr,value in sorted(attrs.items()))
+
+    else:
+        attr_str=''
+    if content:
+        return '\n'.join('<%s%s>%s</%s>'%(name,attr_str,c,name) for c in content)
+    else:
+        return '<%s %s />'%(name,attr_str)
+
+print(tag('br'))
+print(tag('p','hello'))
+print(tag('p','hello','world'))
+print(tag('p','hello',id=33))
+print(tag('p','hello','world',cls='sidebar'))
+
+my_tag={'name':"img",
+        'src':'sunset.jpg','cls':'framed',}
+
+print(tag(**my_tag))
